acf216883d3f963ea8193155fb29c5bc81abee9f
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-14 01:46:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
 # Add  code here!
 def prime?(i)
   if i < 2
-    return false
-    (2..i-1).to_a.any? {|num| i % num == 0} ? false : true
+    false
+    if (2..i-1).to_a.any? {|num| i % num == 0} ? false : true
   end
 end
-
